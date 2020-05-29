f3ceac4818224e9bd583aa85488f4d89c64d6b45
Marco Andrade <marco.andrade@ifro.edu.br>
2020-05-29 13:53:39 -0400

    Alterando endereço

--- html/assets/js/functions.js
+++ html/assets/js/functions.js
@@ -5,7 +5,7 @@ let ativos = document.querySelector("#ativos")
 let descartados = document.querySelector("#descartados")
 let internados = document.querySelector("#internados")
 
-let url = 'https://https://covid19.adsvilhena.ninja/api'
+let url = 'https://apicovid19.adsvilhena.ninja/api'
 
 // Totalizador
 function run(){
